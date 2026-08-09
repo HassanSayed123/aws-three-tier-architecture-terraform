b10b784b11b8b75fd28ab5aa8cead2f8b5c99aa5
HASSAN Elgandy <hassanelgandy113@gmail.com>
2026-08-09 23:43:03 +0300

    Initial commit: AWS 3-Tier Architecture infrastructure with Terraform

--- resources/database/main.tf
+++ resources/database/main.tf
@@ -45,7 +45,7 @@ resource "aws_db_instance" "db" {
   engine_version = "5.7"
 
   # instance_class    = "db.t2.micro"
-  instance_class = "db.t3.micro"
+  instance_class    = "db.t3.micro"
   allocated_storage = 20
   storage_type      = "gp2"
 
